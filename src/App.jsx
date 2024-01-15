bbb1ae31d7820169292f530558ae808dc1fe16c0
Artyem Doronin <doronin.artyem@gmail.com>
2024-01-15 20:49:30 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+function Header() {
   return (
-    <div>
-      <h1>Hello React with changes</h1>
-    </div>
+    <header>
+      <h3>Result university</h3>
+      <span>Тут будет показывать время</span>
+    </header>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Hello React with changes 1</h1>
+      </main>
+    </div>
+  )
+}
